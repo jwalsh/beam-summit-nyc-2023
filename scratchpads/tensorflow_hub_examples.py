d57bd5744695754c8b6df31676d52aa6c5f4a1f9
Jason Walsh <j@wal.sh>
2023-06-13 09:53:24 -0400

    Examples for TensorFlow Hub

---
+++ scratchpads/tensorflow_hub_examples.py
@@ -0,0 +1,9 @@
+  !pip install --upgrade tensorflow_hub
+
+  import tensorflow_hub as hub
+
+  model = hub.KerasLayer("https://tfhub.dev/google/nnlm-en-dim128/2")
+  embeddings = model(["The rain in Spain.", "falls",
+                      "mainly", "In the plain!"])
+
+  print(embeddings.shape)  #(4,128)
